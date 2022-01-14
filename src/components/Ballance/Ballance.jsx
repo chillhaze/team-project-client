2752c2dd4c9edce58384802aa14b18cb91971b8e
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-15 01:35:50 +0200

    add coments

--- src/components/Ballance/Ballance.jsx
+++ src/components/Ballance/Ballance.jsx
@@ -14,49 +14,45 @@ import { useDispatch } from 'react-redux';
 import { setBalanceToState } from 'redux/finance/finance-slice';
 import { useMediaQuery } from 'react-responsive';
 
-
 const Ballance = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
 
-
-    const isMobile = useMediaQuery({
-      query: '(max-width: 767px)',
-    })
-  
-  
   // берем баланс со стейта
   const balance = useSelector(({ finance }) => finance.financeData.ballance);
   const dispatch = useDispatch();
 
   const [curentBalance, setCurentBalance] = useState('');
 
-  // проверка, первый раз вводится баланс или нет, что бы задизейблить кнопку
+  // проверка, первый раз вводится баланс или нет
   const [isBalanceEntered, setIsBalanceEntered] = useState(false);
   useEffect(() => balance && setIsBalanceEntered(true), []);
 
   const handlerChange = e => {
-    const balance = e.target.value.trim()
+    const balance = e.target.value.trim();
     setCurentBalance(balance);
   };
 
   // забрасываем баланс в стейт и на бэк
   const handlerSubmit = e => {
     e.preventDefault();
-    // пока забрасываем только в стейт - дальше будет + пост запрос
-   if(!curentBalance) {
-     return
-   } 
-   dispatch(setBalanceToState(curentBalance));
+    // пока забрасываем только в стейт - дальше будет + пост запрос на бек
+    if (!curentBalance) {
+      return;
+    }
+    dispatch(setBalanceToState(curentBalance));
     setIsBalanceEntered(true);
   };
 
   return (
     <Wrapper>
-
-     {isMobile && <ToReports /> } 
+      {isMobile && <ToReports />}
 
       <BalanceWrapper>
         <Text>Баланс:</Text>
-
+        //Рендерим либо форму (если это первый раз вводим) либо спан с значением
+        баланса
         {!isBalanceEntered ? (
           <Form action="submit" onSubmit={handlerSubmit}>
             <Input
@@ -66,9 +62,7 @@ const Ballance = () => {
               placeholder="00.00 UAH"
               min="1"
             />
-            <Button type="submit">
-              Подтвердить
-            </Button>
+            <Button type="submit">Подтвердить</Button>
           </Form>
         ) : (
           <>
@@ -81,8 +75,7 @@ const Ballance = () => {
           </>
         )}
       </BalanceWrapper>
-          {!isMobile && <ToReports />  }
-
+      {!isMobile && <ToReports />}
     </Wrapper>
   );
 };
