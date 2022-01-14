a44417b4253a403f80f4ad79d1436c83041cc967
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-14 14:25:35 +0200

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './auth/auth-slice';
 import { financeSlice } from './finance/finance-slice';
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer,
+    auth: persistReducer(authPersistConfig, authSlice.reducer),
     finance: financeSlice.reducer,
   },
 
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
   devTools: process.env.NODE_ENV === 'development',
 });
