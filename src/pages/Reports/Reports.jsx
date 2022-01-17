0b3febc0f9ea1c8ad0928aba5dd18ce6e2e26546
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-17 19:54:09 +0200

    add back to main branch

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,7 +1,13 @@
+import BackToMain from './BackToMain';
 import { Wrapper } from './Reports.styled';
 
 const Reports = () => {
-  return <Wrapper>Отчеты</Wrapper>;
+  return (
+    <Wrapper>
+      <BackToMain />
+      Отчеты
+    </Wrapper>
+  );
 };
 
 export default Reports;
