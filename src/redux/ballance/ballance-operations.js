d6d7936cdeb077f88f74af33fdeeb1557ca46c4d
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-21 16:37:08 +0200

    fixUpdate

--- src/redux/ballance/ballance-operations.js
+++ src/redux/ballance/ballance-operations.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
 axios.defaults.baseURL = 'https://kapusta-app-teamproject.herokuapp.com/api/';
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
 
 export const getBallance = createAsyncThunk(
   'ballance/get',
-  async credentials => {
+  async (credentials, { getState }) => {
     try {
+      const tkn = getState().auth.user.token;
+      token.set(tkn);
       const { data } = await axios.get('/balance', credentials);
 
       console.log(data.result);
@@ -19,7 +30,9 @@ export const getBallance = createAsyncThunk(
 
 export const createBallance = createAsyncThunk(
   'ballance/post',
-  async credentials => {
+  async (credentials, { getState }) => {
+    const tkn = getState().auth.user.token;
+    token.set(tkn);
     try {
       const { data } = await axios.post('/balance', {
         value: `${credentials}`,
