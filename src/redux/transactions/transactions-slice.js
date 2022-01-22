37e10b796562ec7dbc292bbff2753f46eb7c603f
Sasha Havrylov <sashagavrilov1995@gmail.com>
2022-01-23 00:36:58 +0200

    логика балланса перенесена в редюсер транзакций

--- src/redux/transactions/transactions-slice.js
+++ src/redux/transactions/transactions-slice.js
@@ -2,10 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import * as transactionsOperations from './transactions-operations.js';
 
 const initialState = {
+  ballanceData: null,
   transactionsData: [],
-  ballance: 0,
   type: 'credit',
-  // token: null,
   isLoading: false,
 };
 
@@ -14,7 +13,7 @@ export const transactionsSlice = createSlice({
   initialState,
   reducers: {
     setBalanceToState: (state, action) => {
-      state.ballance = action.payload;
+      state.ballanceData = action.payload;
     },
     setType: (state, action) => {
       state.type = action.payload;
@@ -30,19 +29,18 @@ export const transactionsSlice = createSlice({
       state.token = action.payload.token;
       state.isLoading = false;
     },
-    // [transactionsOperations.getBalance.pending](state, _) {
-    //   state.isLoading = true;
-    // },
-    // [transactionsOperations.getBalance.fulfilled](state, action) {
-    //   state.transactionsData.ballance = action.payload;
-    // state.token = action.payload.token;
-    // state.isLoading = false;
-    // },
-    // [transactionsOperations.setBalance.pending](state, action) {
-    //   state.transactionsData.ballance = action.payload;
-    // state.token = action.payload.token;
-    // state.isLoading = true;
-    // },
+    //------------------ Get Data
+    [transactionsOperations.getBallance.pending](state, _) {
+      state.isLoadingBallance = true;
+    },
+    [transactionsOperations.getBallance.fulfilled](state, action) {
+      state.ballanceData = action.payload;
+      state.isLoadingBallance = false;
+    },
+    [transactionsOperations.createBallance.fulfilled](state, action) {
+      state.ballanceData = action.payload;
+      state.isLoadingBallance = false;
+    },
     [transactionsOperations.addTransaction.fulfilled](state, action) {
       state.transactionsData = [
         ...state.transactionsData,
@@ -62,4 +60,3 @@ export const transactionsSlice = createSlice({
 });
 
 export const { setBalanceToState, setType } = transactionsSlice.actions;
-
